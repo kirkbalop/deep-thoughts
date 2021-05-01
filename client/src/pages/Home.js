cb200ee9e91b9b4a7e8911d81faaf061a20f9927
Kirk <kirkbalop@gmail.com>
2021-05-01 11:33:27 -0700

    added authentication and updated routing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHTS } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
+import { QUERY_ME, QUERY_ME_BASIC } from '../utils/queries';
+import FriendList from '../components/FriendList';
+
+import Auth from '../utils/auth';
 
 const Home = () => {
   // useQuery hook to make every request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
+  // use object destructuring to extract `data` from the `useQuery` hook's response and rename it  `userData` to be more descriptive
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+
+  const loggedIn = Auth.loggedIn();
 
   const thoughts = data?.thoughts || [];
   console.log(thoughts)
@@ -13,13 +21,22 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
             <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
           )}
         </div>
+        {loggedIn && userData ? (
+          <div className = 'col-12 col-lg-3 mb-3'>
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friend={userData.me.friends}
+              />
+            </div>
+        ) : null}
       </div>
     </main>
   );
